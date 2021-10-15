b97ea873f2e011a5372d9de78087104d88962643
Nate <natemausert@gmail.com>
2021-10-15 10:40:26 -0400

    blank with guidelines for js

---
+++ app.js
@@ -0,0 +1,23 @@
+/*----------------- Constants -----------------*/
+
+/*------------- Variables (state) -------------*/
+
+/*--------- Cached Element References ---------*/
+
+/*-------------- Event Listeners --------------*/
+
+/*----------------- Functions -----------------*/
+
+//Structure example vvvvv
+// function start(){
+//   dialogBox.innerText = `It's a dark and stormy night. There's two paths to get into the castle before you.`
+//   button1.innerText = `Climb the gate (risky but fast)`
+//   button2.innerText = `Walk to the back (safe but slow)`
+// }
+
+// function button2Step1(){
+//   button2.eventListener('click'){
+//     dialogBox.innerText = `You lost 10 minutes but got to the back door safely. It's locked.`
+//   }
+// }
+
