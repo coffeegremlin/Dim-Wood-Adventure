9bcaea8a7fc2de7b2007456478d915543712777f
Nate <natemausert@gmail.com>
2021-10-17 16:50:37 -0400

    add all story branches as functions with commented out logic of control flow

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 /*------------- Variables (state) -------------*/
 
-
+const inventory = []
 
 /*--------- Cached Element References ---------*/
 
@@ -17,8 +17,8 @@ const dialog = document.querySelector('#dialog-box')
 // const downBtn = document.querySelector('#down')
 // const leftBtn = document.querySelector('#left')
 
-const choice1 = document.querySelector('#choice1')
-const choice2 = document.querySelector('#choice2')
+const choice1Btn = document.querySelector('#choice1')
+const choice2Btn = document.querySelector('#choice2')
 
 const startBtn = document.querySelector('#start')
 
@@ -31,10 +31,17 @@ const startBtn = document.querySelector('#start')
 // downBtn.addEventListener('click', )
 // leftBtn.addEventListener('click', )
 
-choice1.addEventListener('click', )
-choice2.addEventListener('click', )
+startBtn.addEventListener('click', evt => {
 
-startBtn.addEventListener('click', )
+})
+
+choice1Btn.addEventListener('click', evt => {
+
+})
+choice2Btn.addEventListener('click', evt => {
+
+})
+
 // status.addEventListener('click')
 // statusImg.addEventListener('click', )
 // maybe add some status message? ^^^
@@ -44,6 +51,116 @@ startBtn.addEventListener('click', )
 
 function startGame(){
   startBtn.onclick
+}
+
+// Story branch functions for game vvv
+
+// first choice for player
+function branchStart(){
+
+}
+
+// Dark path choice
+function branch2Dark(){
+
+}
+
+// Player decides to walk back (leads to youAreDead)
+function branch2Walkback(){
+
+}
+
+// Player decides to press on through the dark
+function branch2PressOn(){
+
+}
+
+// Player enters the graveyard
+function branch2Graveyard(){
+  
+}
+
+// Try the gate (branch1 merges with branch 2 here)
+// If player does not have key from branch1, gate === locked/false return to branch2Graveyard
+function branchMergeTryGate(){
+  
+}
+
+// choose church or figure(if key from branch1 === true, leads to church or branchMergeApproachFigure)
+function chooseChurchOrGate(){
+
+}
+
+// Choose church
+function enterChurch(){
+
+}
+
+// Attack the ghost (leads to youAreDead)
+function attackGhost(){
+
+}
+
+// Look for item to remove the ghost (leads to youWin)
+function lookForItem(){
+  
+}
+
+// Approach the cloaked figure (branch1, branch2, and branchMergeTryGate if the player has key from branch 1 lead to this)
+function branchMergeApproachFigure(){
+
+}
+
+// Charge the wizard (leads to youAreDead)
+function chargeWizard(){
+
+}
+
+// Find cover behind gravestone
+function findCover(){
+
+}
+
+// Keep hiding (leads to youAreDead)
+function keepHiding(){
+
+}
+
+// Run for the church (leads to youWin)
+function runToChurch(){
+
+}
+
+
+// Investigate the light/laughter branch
+function branch1Skull(){
+
+}
+
+// Ignore the skull and sneak away (leads to youAreDead)
+function branch1IgnoreSkull(){
+
+}
+
+// Attack the skull (needs to have html injection to give player the silver key === true), and merge branch1 to branchMergeTryGate // branchMergeApproachFigure.
+function branch1AttackSkull(){
+
+}
+
+// END SCREEN FUNCTIONS
+// You are dead screen function (leads to gameover or branchStart)
+function youAreDead(){
+
+}
+
+// You win screen for player (leads to gameover or branchStart)
+function youWin(){
+
+}
+
+// Game over function for end screen
+function gameOver(){
+
 }
 
 
